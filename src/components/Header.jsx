22b0b04ac3637158bc27a1898feeeb66018ea916
Hasindu Ranasinghe <152050830+hasindur@users.noreply.github.com>
2025-08-08 12:40:26 +0530

    edit navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,14 +19,13 @@ function Header() {
             <h1 className='text-3xl  lg:5xl m-0 font-light text-amber-50' >hasindur</h1>
 
             {/* Navigation */}
-            <nav className='hidden md:flex items-center gap-15'>
+            <nav className='hidden md:flex items-center justify-center gap-10'>
                 <a className='text-amber-50 text-base tracking-wider transition-colors hover:text-purple-600 z-50 ' href="#">Home</a>
                 <a className='text-amber-50 text-base tracking-wider transition-colors hover:text-purple-600 z-50  ' href="#">About</a>
                 <a className='text-amber-50 text-base tracking-wider transition-colors hover:text-purple-600 z-50  ' href="#">Projects</a>
                 <a className='text-amber-50 text-base tracking-wider transition-colors hover:text-purple-600 z-50  ' href="#">Contact</a>
             </nav>
 
-            <button className="hidden md:block text-amber-50 text-base bg-purple-600 px-4 py-2 rounded-3xl cursor-pointer tracking-wider transition-colors hover:text-black z-50">Sign In</button>
             <button onClick={toggleMenu} className='md:hidden px2 py-2 rounded-3xl cursor-pointer tracking-wider transition-colors hover:text-black z-50'>
                 <i class='bx  bx-menu bg-black text-amber-50 txt-5xl'  ></i>
             </button>
